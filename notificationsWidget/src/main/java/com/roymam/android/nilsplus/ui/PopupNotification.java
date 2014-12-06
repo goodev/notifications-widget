ae7a1c6f611f0f9e734ca358610614a79d4e9818
roymam <admin@roymam.com>
2014-12-06 13:17:17 +0200

    improvement: keep heads up when clicked, hide it when swiped up

--- notificationsWidget/src/main/java/com/roymam/android/nilsplus/ui/PopupNotification.java
+++ notificationsWidget/src/main/java/com/roymam/android/nilsplus/ui/PopupNotification.java
@@ -8,17 +8,21 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.PixelFormat;
+import android.graphics.Point;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,38 +31,68 @@ import com.roymam.android.notificationswidget.NiLSAccessibilityService;
 import com.roymam.android.notificationswidget.NotificationData;
 import com.roymam.android.nilsplus.ui.theme.Theme;
 import com.roymam.android.nilsplus.ui.theme.ThemeManager;
+import com.roymam.android.notificationswidget.NotificationsService;
 import com.roymam.android.notificationswidget.R;
 import com.roymam.android.notificationswidget.SettingsManager;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PopupNotification {
+public class PopupNotification implements View.OnTouchListener {
     private final String TAG = PopupNotification.class.getName();
     private final SharedPreferences mPrefs;
+    private final int mSlop;
+    private final int mMinFlingVelocity;
+    private final int mMaxFlingVelocity;
+    private final NotificationData mNotification;
     private int mPopupTimeout;
     private Handler mHandler;
     private Theme mTheme;
     private Context mContext;
     private boolean mVisible = false;
-    private LinearLayout mWindowView;
+    private RelativeLayout mWindowView;
     private View mView;
     private WindowManager.LayoutParams mLayoutParams;
     private static List<PopupNotification> queue = new ArrayList<PopupNotification>();
     private final long mAnimationTime = Resources.getSystem().getInteger(android.R.integer.config_shortAnimTime);
 
+    private Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            hide();
+        }
+    };
+    private float mTouchStartY;
+    private float mTouchStartX;
+    private VelocityTracker mVelocityTracker;
+    private boolean mHorizontalMovement;
+    private boolean mVerticalMovement;
+    private int mViewWidth;
+    private int mViewHeight;
+
     private PopupNotification() {
         // default constructor - prevent creating this class without the "create" method
         mPrefs = null;
+        mMinFlingVelocity = 0;
+        mMaxFlingVelocity = 0;
+        mSlop = 0;
+        mNotification = null;
     }
 
-    private PopupNotification(Context context) {
+    private PopupNotification(Context context, NotificationData nd) {
         Log.d(TAG, "PopupNotification");
         mContext = context;
+        mNotification = nd;
         mHandler = new Handler();
         mPrefs = PreferenceManager.getDefaultSharedPreferences(context);
 
-        mWindowView = new LinearLayout(context);
+        // read default android constants
+        ViewConfiguration vc = ViewConfiguration.get(mContext);
+        mSlop = vc.getScaledTouchSlop();
+        mMinFlingVelocity = vc.getScaledMinimumFlingVelocity() * 16;
+        mMaxFlingVelocity = vc.getScaledMaximumFlingVelocity();
+
+        mWindowView = new RelativeLayout(context);
         mLayoutParams = new WindowManager.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT,
@@ -80,23 +114,21 @@ public class PopupNotification {
         }
 
         // create listeners
-        mWindowView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getActionMasked() == MotionEvent.ACTION_OUTSIDE)
-                    hide();
-                return false;
-            }
-        });
+        mWindowView.setOnTouchListener(this);
+        mView.setOnTouchListener(this);
 
-        mWindowView.addView(mView, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        Point displaySize = NPViewManager.getDisplaySize(mContext);
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(Math.min(displaySize.x, displaySize.y), LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        mWindowView.addView(mView, params);
 
         mPopupTimeout = 5000;
     }
 
     public static PopupNotification create(final Context context, final NotificationData nd)
     {
-        final PopupNotification pn = new PopupNotification(context);
+        final PopupNotification pn = new PopupNotification(context, nd);
         NotificationAdapter.applySettingsToView(context, pn.mView, nd, 0, pn.mTheme, true);
         pn.mView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -163,12 +195,7 @@ public class PopupNotification {
                         .setListener(null);
                 mVisible = true;
 
-                mHandler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        hide();
-                    }
-                }, mPopupTimeout);
+                mHandler.postDelayed(mHideRunnable, mPopupTimeout);
 
                 return true;
             }
@@ -210,5 +237,103 @@ public class PopupNotification {
     public boolean isVisible()
     {
         return mVisible;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int action = event.getActionMasked();
+
+        switch(action) {
+            case MotionEvent.ACTION_OUTSIDE:
+                hide();
+                return false;
+            case MotionEvent.ACTION_DOWN:
+                // cancel hide action
+                mHandler.removeCallbacks(mHideRunnable);
+
+                // store initial movement values
+                mTouchStartY = event.getRawY();
+                mTouchStartX = event.getRawX();
+                mHorizontalMovement = false;
+                mVerticalMovement = false;
+                mViewWidth = mView.getWidth();
+                mViewHeight = mView.getHeight();
+
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                mVelocityTracker = VelocityTracker.obtain();
+                mVelocityTracker.addMovement(event);
+
+                float currY = event.getRawY();
+                float currX = event.getRawX();
+
+                if (!mVerticalMovement && Math.abs(currX - mTouchStartX) > mSlop) mHorizontalMovement = true;
+                if (!mHorizontalMovement && Math.abs(currY - mTouchStartY) > mSlop) mVerticalMovement = true;
+
+                if (mVerticalMovement) {
+                    if (currY < mTouchStartY) {
+                        mView.setTranslationY(currY - mTouchStartY);
+                    } else
+                        mView.setTranslationY(0);
+                }
+                else if (mHorizontalMovement) {
+                    mView.setTranslationX(currX - mTouchStartX);
+                    mView.setAlpha(1 - Math.abs(currX - mTouchStartX) / mViewWidth);
+                }
+
+                break;
+            case MotionEvent.ACTION_UP:
+                currY = event.getRawY();
+                currX = event.getRawX();
+                mVelocityTracker.addMovement(event);
+                mVelocityTracker.computeCurrentVelocity(1000);
+                float velocityX = mVelocityTracker.getXVelocity();
+                float velocityY = mVelocityTracker.getYVelocity();
+                float deltaX = currX - mTouchStartX;
+                float deltaY = currY - mTouchStartY;
+
+                if (mVerticalMovement) {
+
+                    if (currY < mTouchStartY) {
+                        mView.setTranslationY(deltaY);
+                    } else
+                        mView.setTranslationY(0);
+
+
+                    if (velocityY > mMinFlingVelocity && velocityY < mMaxFlingVelocity ||
+                        deltaY < - mViewHeight / 2)
+                        hide();
+                    else
+                        mView.animate().translationY(0).setListener(null);
+                }
+                else if (mHorizontalMovement) {
+                    if (velocityX > mMinFlingVelocity && velocityX < mMaxFlingVelocity ||
+                        Math.abs(deltaX) > mViewWidth / 2)
+                        mView.animate().translationX(mViewWidth).alpha(0).setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                // clear notification
+                                NotificationsService ns = NotificationsService.getSharedInstance();
+                                if (ns != null)
+                                    ns.clearNotification(mNotification.getUid());
+                                // hide popup
+                                hide();
+                            }
+                        });
+                    else
+                        mView.animate().translationX(0).alpha(1).setListener(null);
+                }
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                if (mVelocityTracker != null) {
+                    mVelocityTracker.recycle();
+                    mVelocityTracker = null;
+                }
+                // reset position and opacity
+                mView.animate().translationX(0).translationY(0).alpha(1).setListener(null);
+                break;
+        }
+
+        return false;
     }
 }
